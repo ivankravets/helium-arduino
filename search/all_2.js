13684a4156dcdebce478c28293ffcb9838e266c9
Marc Nijdam <marc@helium.com>
2017-06-23 10:15:35 -0700

    Generate docs

--- search/all_2.js
+++ search/all_2.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['helium',['Helium',['../class_helium.html',1,'Helium'],['../class_helium.html#ac89c58a91a0f0b23a498fe145ea81e3e',1,'Helium::Helium(HardwareSerial *serial)'],['../class_helium.html#a54b5d33944d6e23d2bf65e6c09690920',1,'Helium::Helium(SoftwareSerial *serial)']]]
+  ['helium',['Helium',['../class_helium.html',1,'Helium'],['../class_helium.html#ac89c58a91a0f0b23a498fe145ea81e3e',1,'Helium::Helium(HardwareSerial *serial)'],['../class_helium.html#a54b5d33944d6e23d2bf65e6c09690920',1,'Helium::Helium(SoftwareSerial *serial)']]],
+  ['helium_20for_20arduino',['Helium for Arduino',['../index.html',1,'']]]
 ];
--- search/searchdata.js
+++ search/searchdata.js
@@ -2,20 +2,23 @@ var indexSectionsWithContent =
 {
   0: "bchips",
   1: "ch",
-  2: "bchips"
+  2: "bchips",
+  3: "h"
 };
 
 var indexSectionNames =
 {
   0: "all",
   1: "classes",
-  2: "functions"
+  2: "functions",
+  3: "pages"
 };
 
 var indexSectionLabels =
 {
   0: "All",
   1: "Classes",
-  2: "Functions"
+  2: "Functions",
+  3: "Pages"
 };
 
